54dd433474e8a55fe77424ac6adee24652271a15
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-02-26 12:59:49 +0000

    Deploying to gh-pages from @ LightJack05/CrunchBound@782f6eaf8ea4566e215d5c0158cf46ff8309c249 🚀

--- docs/df/d0a/main_8cpp.js
+++ docs/df/d0a/main_8cpp.js
@@ -1,5 +1,6 @@
 var main_8cpp =
 [
+    [ "CleanupBeforeExit", "df/d0a/main_8cpp.html#aed6a965c795e2202196ce9dc698d8046", null ],
     [ "frame", "df/d0a/main_8cpp.html#ae8fa368bbea5177612cd2cf333448c66", null ],
     [ "gameLoop", "df/d0a/main_8cpp.html#abfbee20b241e7d5bbbef9ab36fb201a4", null ],
     [ "main", "df/d0a/main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4", null ]
